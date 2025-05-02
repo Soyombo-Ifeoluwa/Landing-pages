df2de23f13c3db44fc66fc29480be884ee997773
Soyombo Ifeoluwa <soyomboifeoluwa90@gmail.com>
2025-05-03 00:01:27 +0100

    new

---
+++ src/LandingPage 1/Hero/Hero.js
@@ -0,0 +1,56 @@
+import "./Hero.css";
+import png from "./Frame1.png";
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="navbar-container">
+        <div className="logo-container">
+          <h1>EduFlex</h1>
+        </div>
+        <nav className="navbar">
+          <div>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Courses</a>
+            <a href="#">Contact</a>
+          </div>
+        </nav>
+        <div className="sidenav">
+          <a href="#">Sign Up</a>
+        </div>
+      </div>
+      <div className="hero-content">
+        <div className="hero-text">
+          <h1>A new way to learn & get knowledge.</h1>
+          <p>
+            EduFlex is here for you with various courses & materials from
+            skilled tutors all around the world.
+          </p>
+          <div className="buttons">
+            <button className="hero-button">Join the Class</button>
+            <button className="hero-button1">Learn More</button>
+          </div>
+          <div className="hero-stats">
+            <div>
+               <span>50.5K+</span>
+              <p>Active students</p>
+            </div>
+            <div>
+               <span>1.2K+</span>
+              <p>Tutors</p>
+            </div>
+            <div>
+              <span> 500+</span>
+              <p>Courses</p>
+            </div>
+          </div>
+        </div>
+        <div className="hero-image">
+          <img src={png} alt="He" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/LandingPage 1/Page1.js
@@ -0,0 +1,13 @@
+import About from "./About/About";
+import Hero from "./Hero/Hero";
+function Page1() {
+  return (
+    <div>
+     <Hero />
+     <About/>
+    </div>
+  );
+}
+
+
+export default Page1;
